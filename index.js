e5d5dec37214e38d3887a51829e6655f4ce3bba4
DrownUnity <pabloesaaray@gmail.com>
2024-04-16 19:23:53 -0400

    Hello World

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hola mundo");
+const express = require("express")
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo")
+})
+
+app.listen(port, () => {
+  console.log("Mi port " + port);
+})
